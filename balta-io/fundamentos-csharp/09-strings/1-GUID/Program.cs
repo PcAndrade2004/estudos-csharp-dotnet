8864d6038d794fc0b36df68dbc4ae14ea9973273
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-06-22 19:34:25 -0300

    Estudo - GUID

---
+++ balta-io/fundamentos-csharp/09-strings/1-GUID/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("==== GUID ====\n");
+
+// Em diversos momentos precisamos identificar alguem seja com (id) ou os (GUIDS)
+
+var ID = Guid.NewGuid();
+Console.WriteLine(ID);
+
+Console.WriteLine($"Guid Padrão:  {ID.ToString("N")}");
+
+Console.WriteLine($"Guid Sem Hífens:  {ID.ToString("N")}");
+
+Console.WriteLine($"Guid com Chaves:  {ID.ToString("B")}");
+
+string texto = "dsadasdasd11221eqwd12";
+
+Guid id = Guid.Parse(texto);
+
+Console.WriteLine(id);
+
+
+
+
